eac77467592a75d21f6b667e63b4376b56f60f18
Przemysław Walkiewicz <przemyslaw.walkiewicz@o2.pl>
2018-09-16 10:06:51 +0200

    selectAllEmployeesToBranch

---
+++ selectAllFromTableCustomersEmployee.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM customers_employee
+LIMIT 0, 1000
+
+-- Date: 2018-09-16 10:05
+*/
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (1,'Scooby','Doo',111111111,'Dog',1);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (2,'Shaggy','Roges',222222222,'guy who is afraid of everything',1);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (3,'Fred','Jones',333333333,'Brave man',1);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (4,'Daphne','Blake',444444444,'The preety girl in pack of frends',1);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (5,'Velma','Dinkley',555555555,'The smartest lady in team',1);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (6,'George','Jetson',666666666,'The Father',3);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (7,'Jane','Jetson',777777777,'The Mother',3);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (8,'Judy','Jetson',888888888,'The Daughter',3);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (9,'Elroy','Jetson',999999999,'The Son',3);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (10,'Daffy','Duck',10101010,'Funny duck',2);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (11,'Porky','Pig',11111111,'Pig whose is afraid of everything',2);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (12,'Bugs','Bunny',12121212,'The main character?',2);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (13,'Lola','Bunny',13131313,'My first love, did you see Space Jam?! :)',2);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (14,'Sylvester','Cat',14141414,'He want to catch Tweety, but he is too smart',2);
+INSERT INTO `customers_employee` (`id`,`name`,`surname`,`phone`,`comment`,`CUSTOMERS_COMPANY_id`) VALUES (15,'TWEETY','',15151515,'Smart bird',2);
